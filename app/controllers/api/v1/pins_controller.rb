4966e721bbfb8969017016f0fc1b1d3343beaf2a
ejaramillom <ejaramilo@hotmail.com>
2020-04-17 18:02:23 -0500

    try without changes

--- app/controllers/api/v1/pins_controller.rb
+++ app/controllers/api/v1/pins_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::PinsController < ApplicationController
 
-  before_action :authenticate
+  # before_action :authenticate
 
   def index
     render json: Pin.all.order('created_at DESC')
@@ -20,10 +20,10 @@ class Api::V1::PinsController < ApplicationController
       params.require(:pin).permit(:title, :image_url)
     end
 
-    def authenticate
-      email = request.headers['X-User-Email']
-      token = request.headers['X-Api-Token']
-      permision = User.exists?(api_token: request.headers['X-Api-Token'], email: request.headers['X-User-Email'])
-      head :unauthorized unless permision
-    end
+    # def authenticate
+    #   email = request.headers['X-User-Email']
+    #   token = request.headers['X-Api-Token']
+    #   permision = User.exists?(api_token: request.headers['X-Api-Token'], email: request.headers['X-User-Email'])
+    #   head :unauthorized unless permision
+    # end
 end
